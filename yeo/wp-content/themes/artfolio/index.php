a9b86c49d47261b76b7258ac501ec58e9b5d8400
Kayleigh Kowatsch <kowatsch_22@hotmail.com>
2015-12-07 22:38:19 -0500

    Portfolio
    A one-page portfolio for a client.

---
+++ yeo/wp-content/themes/artfolio/index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Blair Yeomans</title>
+		<meta name="description" content="art + surreal + portfolio">
+		<link rel="stylesheet" href="css/style.css">
+		<!-- IE6-8 support of HTML5 elements --> <!--[if lt IE 9]>
+		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+<body>
+<div id="wrapper">
+<header>
+<hgroup>
+<h1>Blair Yeomans</h1>
+<h3>Surreal Art</h3>
+</hgroup>
+<nav>
+<ul>
+<li><a href="#">Home</a></li>
+<li><a href="#">About HTML5</a></li>
+</ul>
+</nav>
+</header>
+<section>
+</section>
+<footer>
+<div class="footer_navigation">
+<ul>
+<li><a href="#">Home</a></li>
+<li><a href="#">About HTML5</a></li>
+</ul>
+</div>
+<hgroup>
+<h3>By Dain Miller</h3>
+<h4>from Madison, WI</h4>
+</hgroup>
+<address>
+<a href="mailto:miller.dain@gmail.com">Email Me</a>
+</address>
+</footer>
+</body>
+</html>
